91602740d93c16abe5cffd88cbdd18f404a215d6
Styku0 <styku94@gmail.com>
2018-05-20 21:19:17 +0200

    Deleted unnecessary code
    Deleted "rotate to mouse position" code from Gun Controller.

--- Hotline Bling/Assets/Scripts/GunController.cs
+++ Hotline Bling/Assets/Scripts/GunController.cs
@@ -17,9 +17,7 @@ public class GunController : MonoBehaviour
     {
 
         if (Input.GetButtonDown("Fire1"))
-        {
-            rotateToMousePosition();
-
+        {            
             shoot();
         }
 
@@ -37,25 +35,7 @@ public class GunController : MonoBehaviour
 
     }
 
-    void rotateToMousePosition()
-    {
-        //Get the Screen positions of the object
-        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-
-        //Get the Screen position of the mouse
-        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-        //Get the angle between the points
-        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-
-        //Ta Daaa
-        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-    }
-
-    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
-    {
-        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-    }
+    
 }
 
     
